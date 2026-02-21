9788b970fb90667b10cf67b008ce4514e89c0691
Kyoungwoong <kyy980708@gmail.com>
2026-02-21 22:02:33 +0900

    set configuration spring api server

---
+++ nova-spring/src/main/java/com/example/spring/SpringApp.java
@@ -0,0 +1,21 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class, args);
+    }
+
+    @RestController
+    static class Health {
+        @GetMapping("/health")
+        public String health() {
+            return "OK";
+        }
+    }
+}
